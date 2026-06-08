104a8547298a9a0b50c8d6a3d7e629571cb6bcc9
fluxi1804 <fluxi.1804@gmail.com>
2026-06-07 21:04:50 -0500

    Delete freecodecamp.py

--- freecodecamp.py
+++
@@ -1,4 +0,0 @@
-comprador = "cristhian camilo rojas diaz"
-print(type(comprador)) # este nos sirve para ver el tipo de variable
-venta_de_conos = 12.5
-print(isinstance(venta_de_conos, (int,float))) # comprueba si una variable pertenece a un tipo 
